ae665fc1b6295d69d6f00b28af8f3ad6821b9d40
HyeonsuRyu <hsoo112149@gmail.com>
2026-02-02 06:15:59 +0900

    fix: static_url_path에서 '/' 누락 수정

--- src/__init__.py
+++ src/__init__.py
@@ -5,7 +5,7 @@ from flask import Flask
 load_dotenv()
 
 def create_app():
-    app = Flask(__name__, static_url_path=os.getenv("APP_PATH") + 'static')
+    app = Flask(__name__, static_url_path=os.getenv("APP_PATH") + '/static')
 
     from .views import main_views
     app.register_blueprint(main_views.bp, url_prefix=os.getenv("APP_PATH"))
