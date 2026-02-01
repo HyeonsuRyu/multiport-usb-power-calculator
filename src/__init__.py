5d1651f66dce2b7871b045b0a51c728e619fd4f0
HyeonsuRyu <hsoo112149@gmail.com>
2026-02-02 06:08:11 +0900

    feat: Flask 앱의 static_url_path 및 url_prefix에서 기본값 제거

--- src/__init__.py
+++ src/__init__.py
@@ -5,10 +5,10 @@ from flask import Flask
 load_dotenv()
 
 def create_app():
-    app = Flask(__name__, static_url_path=os.getenv("APP_PATH", "/") + 'static')
+    app = Flask(__name__, static_url_path=os.getenv("APP_PATH") + 'static')
 
     from .views import main_views
-    app.register_blueprint(main_views.bp, url_prefix=os.getenv("APP_PATH", "/"))
+    app.register_blueprint(main_views.bp, url_prefix=os.getenv("APP_PATH"))
 
     return app
 
